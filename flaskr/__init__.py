94847f167462c220b7811959dc5ec1bcec731c40
Andreo Barros <andreo.barros@2ndmarket.com.br>
2021-07-20 14:47:04 -0300

    pytest and coverage

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,6 +29,11 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # blog blueprint
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     from . import db
     db.init_app(app)
 
